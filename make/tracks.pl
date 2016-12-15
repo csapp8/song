4a6223d0ff8a0ca6319c29f0080aab2050af8b09
Mirko <mirko@ninja28.d22.lan>
2016-12-15 11:47:48 -0600

    removed MAKE

--- make/tracks.pl
+++
@@ -1,8 +0,0 @@
-while ($a = <>) {
-	if (!($a =~ m/\s*[\#\;]/)) { 	# Ignore comment lines
-		# match header
-		if ($a =~ m/\s*\d+\s*,\s*\d+\s*,\s*Header\s*,\s*\d+\s*,\s*(\d+)\s*,\s*\d+\s*/) {
-			print($1);
-		}
-	}
-}
